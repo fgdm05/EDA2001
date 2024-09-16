ebb85e9ae210f7c410795bd5d4af1966dc81112b
Felipe Godinho Dal Molin <81940822+fgdm05@users.noreply.github.com>
2024-09-16 15:21:08 -0300

    Adicionando Pesquisa por Interpolação
    Contém:
    - Método por Interpolação (checar contador estatístico);
    - Criar main para o Método de Interpolação.

--- anotacoes/metodos_de_pesquisa.c
+++ anotacoes/metodos_de_pesquisa.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/*
+Experimento
+1 até n = 10k
+repetir algoritmo pelo menos 10x para cada função
+- sequencial
+- sequencial com sentinela
+- binária
+- interpolação
+extrair a média
+colocar num csv
+colocar num gráfico
+*/
+
 int* genArray(int n) {
     srand(time(NULL));
     int i;
@@ -10,7 +24,6 @@ int* genArray(int n) {
     }
     return v;
 }
-
 
 int pesquisaSequencial(int chave, int v[], int n, int* c) {
     int i;
@@ -56,6 +69,27 @@ int pesquisaBinaria(int chave, int v[], int n, int* c) {
     return -1;
 }
 
+int pesquisaInterpolacao(int chave, int v[], int n, int* c) {   
+    int ini = 0, meio, fim = n - 1;
+    while(ini <= fim && chave >= v[ini] && chave <= v[fim]) {
+        (*c) += 3;
+        if(ini == fim) return v[ini] == chave ? ini : -1;
+        (*c) += 2;
+        meio = ini + (((double) (fim - ini) / (v[fim] - v[ini])) * (chave - v[ini]));
+        
+        (*c)++;
+        if(chave == v[meio]) 
+            return meio;
+        else if(chave < v[meio]){
+            (*c)++;
+            fim = meio - 1;
+        } else {
+            ini = meio + 1;
+        }
+    }
+    return -1;
+}
+
 void printarr(int* v, int n) {
     int i; for(i = 0; i < n; i++) {
         printf("%d ", *(v+i));
@@ -66,7 +100,7 @@ int compara(const void* a, const void* b) {
     return *(int*) a - *(int*) b;
 }
 
-int main() {
+void main_antigo() {
     int n = 70;
     int* arr = genArray(n);
     // int arr[] = {1,2,3,4,5,6,7,8,9,10};
@@ -81,6 +115,14 @@ int main() {
 
     // pesquisaSequencial(arr[0], arr, n, count);
     // printf("Melhor caso: %d", *count);
+}
+
+int main() {
+    int n = 10;
+    int* v = gen_array(n);
     
-    
+    printarr(v, n);
+    qsort(v, n, sizeof(int), compara);
+    int c = 0;
+    pesquisaInterpolacao(v[3], v, n, &c);
 }
