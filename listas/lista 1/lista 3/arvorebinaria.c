cf65cc057b19e72bf7e96b6033143fc063cb0175
Felipe Godinho Dal Molin <felipegodinhodalmolin@gmail.com>
2024-09-30 21:00:06 -0300

    Questão 4 - Adição sem ponteiro pai (autopoiético)

--- listas/lista 1/lista 3/arvorebinaria.c
+++ listas/lista 1/lista 3/arvorebinaria.c
@@ -4,28 +4,11 @@
 #include "def_q1.h"
 #include "def_q2.h"
 #include "def_q3.h"
-
-
-
-
-void q4() {
-    Arvore* arvore = cria();
-
-    adiciona_v2(arvore, 4);
-    adiciona_v2(arvore, 2);
-    adiciona_v2(arvore, 8);
-    // adiciona_v2(arvore, 1);
-    // adiciona_v2(arvore, 3);
-    // adiciona_v2(arvore, 8);
-    // adiciona_v2(arvore, 9);
-    // adiciona_v2(arvore, 6);
-    // adiciona_v2(arvore, 5);
-    // adiciona_v2(arvore, 7);
-    printf("END");
-}
+#include "def_q4.h"
 
 int main() {
-
+    q1();
+    q2();
+    q3();
     q4();
-
 }
